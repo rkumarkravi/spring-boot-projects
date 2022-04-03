c124146f2c6b2dfd073ec467c8b86a502f763bf2
Ravi Kumar <rkumark.ravi@gmail.com>
2022-04-03 22:46:03 +0530

    changes for file validation

--- musify-rk/src/main/java/com/rk/musify/controller/UploadController.java
+++ musify-rk/src/main/java/com/rk/musify/controller/UploadController.java
@@ -1,6 +1,10 @@
 package com.rk.musify.controller;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.rk.musify.service.UploadService;
+import com.rk.musify.util.Util;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
-	
+
+	@Value("${user.fileContentTypeExpected}")
+	public String[] fileContentTypes;
 	@Autowired
-	UploadService uploadService; 
-	
+	UploadService uploadService;
+
 	@PostMapping(path = "/file")
-	public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile mf){
-		uploadService.save(mf);
-		return ResponseEntity.ok("Uploaded Successfully!!");
-		
+	public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile mf) {
+		String cntntTypeString=Arrays.toString(fileContentTypes);
+		log.info("files expected:{} , fileName: {}",cntntTypeString,mf.getContentType());
+		String fileContentType = mf.getContentType();
+		if (Arrays.asList(fileContentTypes).stream().anyMatch(x -> x.equals(fileContentType))) {
+			uploadService.save(mf);
+			return ResponseEntity.ok("Uploaded Successfully!!");
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("content type expected is: "+cntntTypeString);
+		}
 	}
 
 }
