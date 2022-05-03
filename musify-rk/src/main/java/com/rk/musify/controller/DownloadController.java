59685f97b937d6cb5690e9141521751f97811606
Ravi Kumar <rkxolo.rk@gmail.com>
2022-05-04 00:57:05 +0530

    changes for song blob donwload to play on FE

--- musify-rk/src/main/java/com/rk/musify/controller/DownloadController.java
+++ musify-rk/src/main/java/com/rk/musify/controller/DownloadController.java
@@ -1,10 +1,19 @@
 package com.rk.musify.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rk.musify.model.dao.MusicBlob;
 import com.rk.musify.service.DownloadService;
 
 @RestController
@@ -14,16 +23,18 @@ public class DownloadController {
 	@Autowired
 	DownloadService downloadService;
 
-	/*
-	 * @GetMapping("/file/{filename}")
-	 * 
-	 * @ResponseBody public ResponseEntity<?> getFile(@PathVariable("filename")
-	 * String filename) { Optional<MusicFile> musicFile =
-	 * downloadService.load(filename); if (musicFile.isPresent()) { return
-	 * ResponseEntity.ok() .header(HttpHeaders.CONTENT_DISPOSITION,
-	 * "attachment; filename=\"" + musicFile.get().getName() + "\"")
-	 * .body(musicFile.get().getBlob()); } else { return
-	 * ResponseEntity.status(HttpStatus.NOT_FOUND).body("File is not present!!"); }
-	 * }
-	 */
+	@GetMapping("/file/{filename}")
+	@ResponseBody
+	public ResponseEntity<?> getFile(@PathVariable("filename") String blobId) {
+		Optional<MusicBlob> musicBlob = downloadService.load(blobId);
+		if (musicBlob.isPresent()) {
+			return ResponseEntity.ok()
+					.header(HttpHeaders.CONTENT_DISPOSITION,
+							"attachment; filename=\"" + musicBlob.get().getId() + "\"")
+					.body(musicBlob.get().getBlob());
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Song not present!!");
+		}
+	}
+
 }
--- musify-rk/src/main/java/com/rk/musify/service/DownloadService.java
+++ musify-rk/src/main/java/com/rk/musify/service/DownloadService.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rk.musify.model.dao.MusicBlob;
 import com.rk.musify.model.dao.MusicFile;
+import com.rk.musify.repository.MusicBlobDao;
 import com.rk.musify.repository.MusicFileDao;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 public class DownloadService {
 
 	@Autowired
-	private MusicFileDao musicFileDao;
+	private MusicBlobDao musicBlobDao;
 
-	public Optional<MusicFile> load(String filename) {
-		Optional<MusicFile> fileDB = null;
-		fileDB = musicFileDao.findById(filename);
+	public Optional<MusicBlob> load(String blobId) {
+		Optional<MusicBlob> fileDB = null;
+		fileDB = musicBlobDao.findById(blobId);
 		log.debug("file is present: {}",fileDB.isPresent());
 		return fileDB;
 	}
